02e022385773dd5c544e1e4daa501eda63e871ba
Francisco Ruiz <fruiz_cruz@outlook.com>
2023-05-15 06:42:25 -0600

    Se crean los servicios para consumir las apis

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { ResponseApi } from '../interfaces/response-api';
+import { Login } from '../interfaces/login';
+import { Usuario } from '../interfaces/usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  private urlAPI:string = environment.endpoint + "Usuario/";  
+  constructor(private http:HttpClient) { }
+    iniciarSesion(request:Login):Observable<ResponseApi>{
+      return this.http.post<ResponseApi>(`${this.urlAPI}IniciasSesion`, request)
+    }
+
+    lista():Observable<ResponseApi>{
+      return this.http.get<ResponseApi>(`${this.urlAPI}Lista`)
+    }
+
+    guardar(request:Usuario):Observable<ResponseApi>{
+      return this.http.post<ResponseApi>(`${this.urlAPI}Guardar`, request)
+    }
+
+    editar(request:Usuario):Observable<ResponseApi>{
+      return this.http.put<ResponseApi>(`${this.urlAPI}Editar`, request)
+    }
+
+    eliminar(id: number):Observable<ResponseApi>{
+      return this.http.delete<ResponseApi>(`${this.urlAPI}Eliminar/${id}`,)
+    }
+}
